6ddc6c65f511263a0fa02a40b539f2b57badc1f3
Danylo Hlynsky <danylohlynsky@gmail.com>
2019-01-31 21:07:31 +0000

    JS-HW1

---
+++ quadratic_equation.js
@@ -0,0 +1,25 @@
+const a = parseFloat(prompt('Enter a'));
+const b = parseFloat(prompt('Enter b'));
+const c = parseFloat(prompt('Enter c'));
+
+
+function discriminant(a, b, c) {
+    return Math.pow(b, 2) - 4 * a * c;
+}
+
+
+function calcX(a, b, c) {
+    if (discriminant(a, b, c) < 0) {
+        return 'Empty set';
+    } else if (discriminant(a, b, c) === 0) {
+        return 'x = ' + (-b) / (2 * a);
+    }
+    else if (discriminant(a, b, c) >= 0) {
+        let x1 = (-b + Math.sqrt(discriminant(a, b, c))) / (2 * a);
+        let x2 = (-b - Math.sqrt(discriminant(a, b ,c))) / (2 * a);
+        return 'x1 = ' + x1 + ' and x2 = ' + x2;
+    }
+}
+
+const result = calcX(a, b, c);
+alert(result);
