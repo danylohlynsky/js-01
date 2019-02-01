948e363a022dc58a9da2696ad5a6a30a62422c6a
Danylo Hlynsky <danylohlynsky@gmail.com>
2019-02-01 18:49:58 +0000

    JS HW2 Update

--- quadratic_equation.js
+++ quadratic_equation.js
@@ -13,8 +13,7 @@ function calcX(a, b, c) {
         return 'Empty set';
     } else if (discriminant(a, b, c) === 0) {
         return 'x = ' + (-b) / (2 * a);
-    }
-    else if (discriminant(a, b, c) >= 0) {
+    } else {
         let x1 = (-b + Math.sqrt(discriminant(a, b, c))) / (2 * a);
         let x2 = (-b - Math.sqrt(discriminant(a, b ,c))) / (2 * a);
         return 'x1 = ' + x1 + ' and x2 = ' + x2;
